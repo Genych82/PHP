75c7c971b7ef664c63d9d6dded0fa7946dc5f80e
Genych82 <118843886+Genych82@users.noreply.github.com>
2022-12-22 18:23:14 +0000

    prac

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 
      $string1 = 'Hello';
 $string2 = 'World!';
-    $text = "$string1". '' . "$string2";
-echo ("$text");
+    $text = "$string1". '+' . "$string2";
+echo ("$text" . "<br/>");
 print ($text);
 
-?>
-<H1 style = "font-size: 85px">
+<H1 style = "font-size: "45px" color: <?="red"?>">
 <?= "Test" ?></H1>
 
 echo htmlentities "hello \"itis"\";
+
+
+CONST pi = 3.14;
+
+$variable = "1";
+
+echo gettype( (int) $variable);
+?>
