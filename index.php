9a6423628fc6c24bf07314ab2e61b9bbf3d0d147
Genych82 <118843886+Genych82@users.noreply.github.com>
2022-12-22 14:44:45 +0000

    php

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?php
 
 
-
-
-function page(){
      $string1 = 'Hello';
 $string2 = 'World!';
     $text = "$string1". '' . "$string2";
 echo ("$text");
 print ($text);
 
-};
-
-page();
-
 ?>
-<H1 style = "font-size: "45px" color: "red" >
+<H1 style = "font-size: 85px">
 <?= "Test" ?></H1>
 
 echo htmlentities "hello \"itis"\";
